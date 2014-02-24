76005699a94432454e61512b9ad3273a8d46cdab
xue <xue@localhost.localdomain>
2014-02-24 13:39:19 +0800

    修改DefaultSmsSender.java 增加DebugSmsSender.java

--- Wario/src/main/java/com/renren/Wario/DefaultSmsSender.java
+++ Wario/src/main/java/com/renren/Wario/DefaultSmsSender.java
@@ -15,33 +15,76 @@
  */
 package com.renren.Wario;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
 
 public class DefaultSmsSender implements ISmsSender {
+	private static Logger logger = LogManager
+			.getLogger(DefaultSmsSender.class.getName());
 
+	private String SERVER_URL = "http://sms.notify.d.xiaonei.com:2000/";
+	
 	/* (non-Javadoc)
 	 * @see com.renren.Wario.ISmsSender#sendSms(java.lang.String, java.lang.String)
 	 */
 	@Override
 	public void sendSms(String telephoneNumber, String smsMessage) {
-		File sms = new File("smsOut.txt");
-		OutputStreamWriter out = null;
-		BufferedWriter writer = null;
+		String url = generateUrl(telephoneNumber, smsMessage);
+		
+		String ret = "";
 		try {
-			out = new OutputStreamWriter(new FileOutputStream(sms));
-			writer = new BufferedWriter(out);
-			writer.append(telephoneNumber + " : " + smsMessage);
-		} catch (FileNotFoundException e) {
+			HttpURLConnection conn = (HttpURLConnection) new URL(url)
+					.openConnection();
+			conn.connect();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					conn.getInputStream()));
+			String tmp = null;
+			while ((tmp = reader.readLine()) != null)
+				ret += tmp;
+			conn.disconnect();
+		} catch (MalformedURLException e) {
+			logger.warn("Number:" + telephoneNumber + " Message:" + smsMessage + " send failed!");
 			e.printStackTrace();
 		} catch (IOException e) {
+			logger.warn("Number:" + telephoneNumber + " Message:" + smsMessage + " send failed!");
 			e.printStackTrace();
+		}
+	}
+	
+	private String generateUrl(String mobilePhone, String msg) {
+		StringBuilder url = new StringBuilder();
+		url.append(SERVER_URL);
+
+		url.append("receiver?number=");
+		url.append(mobilePhone);
+
+		url.append("&message=");
+		url.append(urlEncode(msg));
+
+		return url.toString();
+	}
+	
+	private String urlEncode(String s) throws RuntimeException {
+		try {
+			return URLEncoder.encode(s, "UTF-8").replace(".", "%2E");
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException("This should never happen.");
 		}
 	}
 
