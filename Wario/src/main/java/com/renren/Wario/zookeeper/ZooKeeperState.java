841249f84dbf2af270c5a646663342b9bd443925
x841122987 <heu_xueyan@163.com>
2014-02-28 11:07:27 +0800

    ZooKeeperState.java 增加获取角色功能

--- Wario/src/main/java/com/renren/Wario/zookeeper/ZooKeeperState.java
+++ Wario/src/main/java/com/renren/Wario/zookeeper/ZooKeeperState.java
@@ -1,6 +1,22 @@
+/**
+ *    Copyright 2014 Renren.com
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package com.renren.Wario.zookeeper;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import org.apache.zookeeper.client.FourLetterWordMain;
 
@@ -28,9 +44,15 @@ public class ZooKeeperState {
 		try {
 			statText = cmd("stat");
 			
-			
+			Scanner scanner = new Scanner(statText);
+			while (scanner.hasNext()) {
+				String line = scanner.nextLine();
+				if (line.startsWith("Mode:")) {
+					mode = line.substring(line.indexOf(":") + 1, line.length())
+							.replaceAll(" ", "");
+				}
+			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -41,6 +63,6 @@ public class ZooKeeperState {
 	
 	public static void main(String[] args) throws IOException {
 		ZooKeeperState state = new ZooKeeperState("localhost:21815");
-		
+		System.out.println(state.getMode());
 	}
 }
