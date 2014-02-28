1391e478b351849ca81025bca1125eef813d2dd1
ZheYuan <yizer16@gmail.com>
2014-02-28 17:36:23 +0800

    添加FileReader的Cache机制和测试用例

--- Wario/src/main/java/com/renren/Wario/config/FileReader.java
+++ Wario/src/main/java/com/renren/Wario/config/FileReader.java
@@ -21,6 +21,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -30,31 +33,91 @@ public class FileReader {
 	private static Logger logger = LogManager.getLogger(FileReader.class
 			.getName());
 
-	public static String read(String path) throws IOException {
-		String text = "";
-		File file = new File(path);
-		InputStreamReader in = null;
-		BufferedReader reader = null;
-		try {
-			in = new InputStreamReader(new FileInputStream(file));
-			reader = new BufferedReader(in);
-			String line;
-			while ((line = reader.readLine()) != null) {
-				text = text + line;
-			}
+	private static final class FileCache implements Comparable<FileCache>, Comparator<FileCache>{
+		private final String pathString;
+		private final long lastModified;
+		private final String content;
 
-		} catch (FileNotFoundException e) {
-			logger.error("File not found. path = " + path);
-			e.printStackTrace();
-		} finally {
-			if (reader != null) {
-				reader.close();
+		public FileCache(String pathString) throws IOException {
+			String tmpString = "";
+			File file = new File(pathString);
+			
+			InputStreamReader in = null;
+			BufferedReader reader = null;
+			try {
+				in = new InputStreamReader(new FileInputStream(file));
+				reader = new BufferedReader(in);
+				String line;
+				while ((line = reader.readLine()) != null) {
+					tmpString = tmpString + line;
+				}
+			} catch (FileNotFoundException e) {
+				logger.error("File not found. path = " + pathString);
+				throw e;
+			} finally {
+				content = tmpString;
+				if (reader != null) {
+					reader.close();
+				}
+				if (in != null) {
+					in.close();
+				}
 			}
-			if (in != null) {
-				in.close();
-			}
+			this.pathString = file.getAbsolutePath();
+			lastModified = file.lastModified();
 		}
-		return text;
+		
+		/**
+		 * @return the content
+		 */
+		public String getContent() {
+			return content;
+		}
+		
+		/**
+		 * @return the lastModified
+		 */
+		public long getLastModified() {
+			return lastModified;
+		}
+
+		/**
+		 * @return the pathString
+		 */
+		public String getPathString() {
+			return pathString;
+		}
+
+		/* (non-Javadoc)
+		 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+		 */
+		@Override
+		public int compare(FileCache o1, FileCache o2) {
+			return o1.compareTo(o2);
+		}
+
+		/* (non-Javadoc)
+		 * @see java.lang.Comparable#compareTo(java.lang.Object)
+		 */
+		@Override
+		public int compareTo(FileCache arg0) {
+			return this.getPathString().compareTo(arg0.getPathString());
+		}
+	}
+	
+	private static Map<String, FileCache> fileCache = new HashMap<String, FileCache>();
+
+	public static String read(String path) throws IOException {
+		File file = new File(path);
+		String absolutePath = file.getAbsolutePath();
+		synchronized (fileCache) {
+			FileCache cache = fileCache.get(absolutePath);
+			if (cache == null || cache.getLastModified() != file.lastModified()) {
+				FileCache newCache = new FileCache(absolutePath);
+				fileCache.put(absolutePath, newCache);
+			}
+			return fileCache.get(absolutePath).getContent();
+		}
 	}
 
 }
