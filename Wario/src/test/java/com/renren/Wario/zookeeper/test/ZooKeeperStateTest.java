bbc05e029104272d5311fc0a733f16b98210f0fc
ZheYuan <yizer16@gmail.com>
2014-03-03 21:14:56 +0800

    更新ZooKeeperState测试用例

--- Wario/src/test/java/com/renren/Wario/zookeeper/test/ZooKeeperStateTest.java
+++ Wario/src/test/java/com/renren/Wario/zookeeper/test/ZooKeeperStateTest.java
@@ -2,7 +2,16 @@ package com.renren.Wario.zookeeper.test;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.server.quorum.QuorumPeerMain;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.renren.Wario.zookeeper.ZooKeeperState;
@@ -24,11 +33,36 @@ public class ZooKeeperStateTest {
 			QuorumPeerMain.main(args);
 		}
 	}
-	
+
 	private static ZooKeeperBackgroundServer zkBackgroundServer = null;
-	
+
+	@BeforeClass
+	public static void init() {
+		new File("Wario.log").delete();
+		System.setProperty("default.config.path", "./");
+		PropertyConfigurator
+				.configure(System.getProperty("user.dir") + File.separator
+						+ "conf" + File.separator + "log4j.properties");
+	}
+
 	@Test
-	public void test() {
+	public void test() throws IOException, KeeperException {
+
+		ZooKeeperState state = new ZooKeeperState("localhost:2181");
+		assertEquals(-1, state.getReceived());
+		assertEquals(-1, state.getSent());
+		assertEquals(-1, state.getOutStanding());
+		assertNull(state.getMode());
+		assertEquals(-1, state.getNodeCount());
+		assertEquals(-1, state.getTotalWatches());
+		state.update();
+		assertEquals(-1, state.getReceived());
+		assertEquals(-1, state.getSent());
+		assertEquals(-1, state.getOutStanding());
+		assertNull(state.getMode());
+		assertEquals(-1, state.getNodeCount());
+		assertEquals(-1, state.getTotalWatches());
+
 		if (zkBackgroundServer == null) {
 			zkBackgroundServer = new ZooKeeperBackgroundServer("2181");
 			zkBackgroundServer.setDaemon(true);
@@ -39,16 +73,52 @@ public class ZooKeeperStateTest {
 				e.printStackTrace();
 			}
 		}
-		
-		ZooKeeperState state = new ZooKeeperState("localhost:2181");
-		
+
+		assertEquals(-1, state.getReceived());
+		assertEquals(-1, state.getSent());
+		assertEquals(-1, state.getOutStanding());
+		assertNull(state.getMode());
+		assertEquals(-1, state.getNodeCount());
+		assertEquals(-1, state.getTotalWatches());
+
+		state.update();
 		assertEquals(1, state.getReceived());
 		assertEquals(0, state.getSent());
 		assertEquals(0, state.getOutStanding());
 		assertEquals("standalone", state.getMode());
 		assertEquals(4, state.getNodeCount());
 		assertEquals(0, state.getTotalWatches());
-		
+		ZooKeeper zk = new ZooKeeper("localhost:2181", 5000, null);
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			zk.create("/test", null, Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+		} catch (InterruptedException e) {
+		}
+		state.update();
+		assertEquals(6, state.getReceived());
+		assertEquals(5, state.getSent());
+		assertEquals(0, state.getOutStanding());
+		assertEquals("standalone", state.getMode());
+		assertEquals(5, state.getNodeCount());
+		assertEquals(0, state.getTotalWatches());
+		try {
+			zk.delete("/test", -1);
+		} catch (KeeperException e) {
+		} catch (InterruptedException e) {
+		}
+		state.update();
+		assertEquals(9, state.getReceived());
+		assertEquals(8, state.getSent());
+		assertEquals(0, state.getOutStanding());
+		assertEquals("standalone", state.getMode());
+		assertEquals(4, state.getNodeCount());
+		assertEquals(0, state.getTotalWatches());
 	}
 
 }
