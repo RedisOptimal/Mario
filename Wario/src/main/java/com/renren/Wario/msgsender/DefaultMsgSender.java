2685fcf04cc0b08c5c065e308442c9648b90e5f8
x841122987 <heu_xueyan@163.com>
2014-03-04 14:55:47 +0800

    添加短信发送功能

--- Wario/src/main/java/com/renren/Wario/msgsender/DefaultMsgSender.java
+++ Wario/src/main/java/com/renren/Wario/msgsender/DefaultMsgSender.java
@@ -15,12 +15,72 @@
  */
 package com.renren.Wario.msgsender;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 public class DefaultMsgSender implements IMsgSender {
+
+	private static Logger logger = LogManager.getLogger(DefaultMsgSender.class
+			.getName());
+
+	private final String SERVER_URL = "http://sms.test.com/";
 
 	@Override
 	public void sendMessage(String number, String message) {
-		
-		System.out.println(number + " : " + message);
+
+		String url = generateUrl(number, message);
+
+		String ret = "";
+		try {
+			HttpURLConnection conn = (HttpURLConnection) new URL(url)
+					.openConnection();
+			conn.connect();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					conn.getInputStream()));
+			String tmp = null;
+			while ((tmp = reader.readLine()) != null)
+				ret += tmp;
+			conn.disconnect();
+			logger.info("Number:" + number + " Message:" + message + " return "
+					+ ret + ".");
+		} catch (MalformedURLException e) {
+			logger.warn("Number:" + number + " Message:" + message
+					+ " send failed!");
+			e.printStackTrace();
+		} catch (IOException e) {
+			logger.warn("Number:" + number + " Message:" + message
+					+ " send failed!");
+			e.printStackTrace();
+		}
 	}
 
+	private String generateUrl(String mobilePhone, String msg) {
+		StringBuilder url = new StringBuilder();
+		url.append(SERVER_URL);
+
+		url.append("receiver?number=");
+		url.append(mobilePhone);
+
+		url.append("&message=");
+		url.append(urlEncode(msg));
+
+		return url.toString();
+	}
+
+	private String urlEncode(String s) throws RuntimeException {
+		try {
+			return URLEncoder.encode(s, "UTF-8").replace(".", "%2E");
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException("This should never happen.");
+		}
+	}
 }
