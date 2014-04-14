d7fa2ba32118ad7e6c8fdfaa51f76e4d88b5a475
x841122987 <heu_xueyan@163.com>
2014-04-14 20:17:06 +0800

    删掉无用代码

--- Mario/src/main/java/com/renren/infra/xweb/web/Mario_server_stateController.java
+++ Mario/src/main/java/com/renren/infra/xweb/web/Mario_server_stateController.java
@@ -4,18 +4,13 @@ package com.renren.infra.xweb.web;
 import java.util.Map;
 
 import javax.servlet.ServletRequest;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springside.modules.web.Servlets;
 
 import com.renren.infra.xweb.entity.Mario_server_state;
